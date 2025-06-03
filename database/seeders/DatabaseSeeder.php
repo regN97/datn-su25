9b90a13823b53b498ea6e31459238d9f96b2eff8
DuyMax2005 <duytuyetdinh@gmail.com>
2025-06-03 09:53:03 +0700

    Đã tạo dữ liệu mẫu đơn đặt hàng

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
             ProductUnitSeeder::class,
             ProductSeeder::class,
             ProductSupplierSeeder::class,
-
+            PurchaseOrderSeeder::class,
         ]);
     }
 }
