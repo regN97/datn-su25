a1818894f11953dd68abdcfa64ba69345ba2af6c
regNiqa <regniquanh@gmail.com>
2025-06-08 15:00:36 +0700

    resolve conflicts

--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -48,16 +48,14 @@ class PurchaseOrder extends Model
     {
         return $this->belongsTo(User::class, 'approved_by');
     }
-<<<<<<< HEAD
 
     public function items()
     {
         return $this->hasMany(PurchaseOrderItem::class);
-=======
+    }
     
     public function purchaseReceipts()
     {
         return $this->hasMany(PurchaseReceipt::class);
->>>>>>> 86c3ddb (edit:purchase_receipt)
     }
 }
